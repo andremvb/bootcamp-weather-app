f3ed214a1e174dec17ad9545e97deb4262190fe9
T02909 <andrevaldivia@bcp.com.pe>
2024-10-03 15:36:53 -0500

    Initial commit

---
+++ bootcamp-weather-app/bootcamp_weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  bootcamp_weather_appApp.swift
+//  bootcamp-weather-app
+//
+//  Created by Andre Valdivia on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct bootcamp_weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
