2de26ee9a9f849203b7e75d06d9ca80f50cede78
T02909 <andrevaldivia@bcp.com.pe>
2024-10-03 18:06:29 -0500

    Address TODOs

--- bootcamp-weather-app/WeatherService/CurrentWeather.swift
+++ bootcamp-weather-app/WeatherService/CurrentWeather.swift
@@ -20,7 +20,6 @@ struct Location: Decodable {
     let lat: Double
     let lon: Double
     let tzId: String
-    // Using camelCase for consistency
     let localtimeEpoch: Int
     let localtime: String
 
@@ -41,33 +40,9 @@ struct APIWeather: Decodable {
   let lastUpdated: String
   let tempC: Double
   let tempF: Double
-//  let isDay: Bool
   let condition: WeatherCondition
   let windMph: Double
-//  let windKph: Double
-//  let windDegree: Int
-//  let windDir: String
-//  let pressureMb: Double
-//  let pressureIn: Double
-//  let precipMm: Double
-//  let precipIn: Double
   let humidity: Int
-//  let cloud: Int
-//  let feelslikeC: Double
-//  let feelslikeF: Double
-//
-//  let windchillC: Double
-//  let windchillF: Double
-//  let heatindexC: Double
-//  let heatindexF: Double
-//  let dewpointC: Double
-//  let dewpointF: Double
-//
-//  let visKm: Double
-//  let visMiles: Double
-//  let uv: Double
-//  let gustMph: Double
-//  let gustKph: Double
 
     enum CodingKeys: String, CodingKey {
         case lastUpdatedEpoch = "last_updated_epoch"
--- bootcamp-weather-app/WeatherService/WeatherService.swift
+++ bootcamp-weather-app/WeatherService/WeatherService.swift
@@ -8,28 +8,35 @@
 import Foundation
 
 struct WeatherService {
+
+    enum ServiceError: Error {
+        case badUrl
+        case badDateConversion
+    }
+
     private let token = "71592f6f219c4dc2bf8202603240310"
     private let session = URLSession.shared
     private let baseURL = "https://api.weatherapi.com/v1/"
 
     func fetchCity(_ city: String) async throws -> City {
-        // TODO make this calls not waiting ? :v
         let currentData = try await fetchCurrent()
-        // TODO: Unhardcode dates
-        let tomorrowData = try await fetchFuture(date: "2024-11-04")
-        let oneDayAfterData = try await fetchFuture(date: "2024-11-05")
+        let tomorrowData = try await fetchFuture(date: dateAfter(days: 1))
+        let oneDayAfterDateString = try dateAfter(days: 2)
+        let oneDayAfterData = try await fetchFuture(date: oneDayAfterDateString)
 
-        // TODO: Unhardcode
         let currentWeather = Weather(
             apiWeather: currentData.current,
             date: "Today"
         )
         let tomorrowWeather = Weather(futureWeather: tomorrowData, date: "Tomorrow")
-        let oneDayAfterWeather = Weather(futureWeather: oneDayAfterData, date: "Friday")
+        let oneDayAfterWeather = Weather(
+            futureWeather: oneDayAfterData,
+            date: try getDayName(dateString: oneDayAfterDateString)
+        )
 
         let date = Date()
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = ""
+        dateFormatter.dateFormat = "EEEE, MMM d, yyyy"
         dateFormatter.string(from: date)
 
         return City(
@@ -48,14 +55,14 @@ struct WeatherService {
 
     private func fetchCurrent() async throws -> CurrentWeather {
         let string = baseURL + "current.json"
-        // TODO: Remove !
-        var components = URLComponents(string: string)!
+        guard var components = URLComponents(string: string) else { throw ServiceError.badUrl }
         components.queryItems = [
             URLQueryItem(name: "key", value: token),
             URLQueryItem(name: "q", value: "San Francisco"),
             URLQueryItem(name: "aqi", value: "no")
         ]
-        let (data, _ ) = try await session.data(from: components.url!)
+        guard let url = components.url else { throw ServiceError.badUrl }
+        let (data, _ ) = try await session.data(from: url)
         let decoder = JSONDecoder()
         let currentWeather = try decoder.decode(CurrentWeather.self, from: data)
         return currentWeather
@@ -64,17 +71,38 @@ struct WeatherService {
     // date format = 2024-11-02
     private func fetchFuture(date: String) async throws -> FutureWeather {
         let string = baseURL + "future.json"
-        // TODO: Remove !
-        var components = URLComponents(string: string)!
+        guard var components = URLComponents(string: string) else { throw ServiceError.badUrl }
         components.queryItems = [
             URLQueryItem(name: "key", value: token),
             URLQueryItem(name: "q", value: "San Francisco"),
             URLQueryItem(name: "dt", value: date)
         ]
-        let (data, _ ) = try await session.data(from: components.url!)
-//        print(String(data: data, encoding: .utf8))
+        guard let url = components.url else { throw ServiceError.badUrl }
+        let (data, _ ) = try await session.data(from: url)
         let decoder = JSONDecoder()
         let futureWeather = try decoder.decode(FutureWeather.self, from: data)
         return futureWeather
+    }
+
+    private func dateAfter(days: Int) throws -> String {
+        let today = Date()
+        let calendar = Calendar.current
+        guard let tomorrow = calendar.date(byAdding: .day, value: days, to: today) else {
+            throw ServiceError.badDateConversion
+        }
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        return dateFormatter.string(from: tomorrow)
+    }
+
+    private func getDayName(dateString: String) throws -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        guard let date = dateFormatter.date(from: dateString) else {
+            throw ServiceError.badDateConversion
+        }
+        let dayNameFormatter = DateFormatter()
+        dayNameFormatter.dateFormat = "EEEE"
+        return dayNameFormatter.string(from: date)
     }
 }
